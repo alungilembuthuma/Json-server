757318864e67e6645554c5637600f84e6768b0b1
alungilembuthuma <alungilembuthuma0320@gmail.com>
2024-07-12 10:31:59 +0200

    feat:added branch dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const car = {type:"Fiat", model:"500", color:"white"};
+  const [FileList,setList]=useState({});
 
+useEffect(() => {
+ console.log("effect run")
+ const fetchtraineesData = async() =>{
+  console.log ("running fetchTraineeesData")
+
+  try{ 
+    const response = await fetch(localhost4000/trainees)
+ console.log("Below is the trainee data", traineesData);
+
+
+  }catch (error){
+    alert(error);                       
+  }
+};
+
+(async () => await fetchtraineesData())()
+ }
+)
+  const trainee ={ name:"Alungile", surname:"Mbuthuma", age:"21", color:"Pink",gender:"female", location:"Umlazi"}
+  const traineeData = "traineesDataURL"
+  const traineesData = await.response.json();
+  setList(traineesData);
+  console.log("Below is the trainees data,traineesData");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<h1>My name is {trainee.name}</h1>
+<p>I live at {trainee.location}, and my favorite color is {trainee.color}.</p>
     </>
   )
 }
